d500ee8fd834334d8446421814f17fccaf47a35b
Miqweaschi <nazzarotessitore48@gmail.com>
2025-12-05 13:40:10 +0100

    aggiunto il compilatore

---
+++ TableTalk/TableTalk/TableTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableTalkApp.swift
+//  TableTalk
+//
+//  Created by AFP PAR 21 on 05/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TableTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
