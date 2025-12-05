fd277ad2b25aa28f57e65747e37df8900ea8f47d
Matteo <matteoromano64@icloud.com>
2025-12-05 15:06:26 +0100

    Aggiornata la mainHomeView

--- TableTalk/Model/Model.swift
+++ TableTalk/Model/Model.swift
@@ -5,3 +5,16 @@
 //  Created by AFP PAR 21 on 05/12/25.
 //
 
+import Foundation
+
+struct Utente {
+    
+    let name : String
+    
+    func getName() -> String {
+        return name
+    }
+    
+   
+    
+}
--- TableTalk/View/MainHomeView.swift
+++ TableTalk/View/MainHomeView.swift
@@ -9,16 +9,27 @@ import Foundation
 import SwiftUI
 
 struct MainHomeView: View {
+   
+  
+   @State  var nome : Utente = Utente(name: "")
+   @State  var isAuthonticate : Bool = false
+    
     var body: some View {
             VStack {
-               Text("Welcome Back, Christian")
+                Text("Welcome Back!")
                     .font(Font.largeTitle)
+                    .foregroundColor(.white)
                     .frame(maxWidth: .infinity)
                     .padding(.bottom,20)
                     .padding(.top,50)
-                    .background(Color(red: 0.70, green: 0.0, blue: 0.1))
+                    .background(Color(red: 0.800, green: 0.0, blue: 0.1))
+                    .shadow(color:.black,    radius : 0)
+                
                     Spacer()
-        }
+            }
+            .background(Color.yellow.opacity(0.1))
+        
+        
     }
 }
 struct ContentView_Previews: PreviewProvider {
