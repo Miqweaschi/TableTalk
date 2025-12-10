fc522158f74bf2e79a1356657e50cfb69a77c072
francesco <ecrasevvv@gmail.com>
2025-12-10 15:14:47 +0100

    Create view per gestiere i singoli argomenti delle singole lezioni

---
+++ TableTalk/View/ArgomentoView.swift
@@ -0,0 +1,18 @@
+//
+//  ArgomentoView.swift
+//  TableTalk
+//
+//  Created by AFP PAR 32 on 10/12/25.
+//
+
+import SwiftUI
+
+struct ArgomentoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArgomentoView()
+}
