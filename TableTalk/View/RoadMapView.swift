9b95597fcc25d294ffdd076b9f491056a18f8281
S1GMA777 <politoantonio777@gmail.com>
2025-12-10 13:52:21 +0100

    roadmap provvisoria

--- TableTalk/View/RoadMapView.swift
+++ TableTalk/View/RoadMapView.swift
@@ -5,6 +5,20 @@ import UIKit
 
 struct RoadMapView: View {
     var body: some View {
+            VStack
+        {
+            Text("RoadMap")
+                .font(.largeTitle)
+                .padding()
+            
+            List {
+                Text("Item 1")
+                Text("Item 2")
+                Text("Item 3")
+                
+            }
+            
+        }
     }
 }
 
