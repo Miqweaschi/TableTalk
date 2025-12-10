f33be12b5d501e79b97ba77441fa64f1c74ea2a6
francesco <ecrasevvv@gmail.com>
2025-12-10 11:34:13 +0100

    rimossi commenti inutili

--- TableTalk/View/MainHomeView.swift
+++ TableTalk/View/MainHomeView.swift
@@ -7,17 +7,13 @@ struct MainHomeView: View {
     @State  var nome : Utente = Utente(name: "")
     @State  var isAuthonticated : Bool = false
     
-    //selectedDate è una variabile che contiene la data corrente.
-    // ci servirà nella funzione DatePicker come variabile in input
-    // utile a cerchiare sul calendario il giorno corrente.
+    // Contiene la data corrente
     @State private var selectedDate = Date()
     
-    //progress è utile come variabile perchè contiene il valore
-    // della progression bar(Gauge), progress va da 0.0 a 1 dove 0.0 è lo 0% mentre 1.0 è il 100%.
+    // Valore numerico del progresso
     @State private var progress: Double = 0.2
     
     var body: some View {
-        //Questa VStack rappresenta il blocco rosso in alto con la scritta "Welcome Back!"
         VStack {
             Text("Welcome Back!")
                 .font(Font.largeTitle)
@@ -56,14 +52,10 @@ struct MainHomeView: View {
             
             //DatePicker è una funzione che ci permette di implementare il calendario
             DatePicker("", selection: $selectedDate, displayedComponents: [.date])
-            //.datePickerStyle impostandolo a graphical ci permette di
-            // mostrarlo in primo piano, ci sono un altro paio di opzioni
-            // come per esempio la visualizzazione a "ruota" ma a noi interessa questa.
                 .datePickerStyle(.graphical)
                 .padding(.top,10)
                 .tint(Color(r:182,g:23,b:45,opacity:100))
                 .offset(x: 0, y: -20)
-            
             Spacer()
             
         }
@@ -71,11 +63,8 @@ struct MainHomeView: View {
     }
 }
 
-
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-
