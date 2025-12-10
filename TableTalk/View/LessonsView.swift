b80bea36c7ef0c9b133017077127a3ba07167b54
Miqweaschi <nazzarotessitore48@gmail.com>
2025-12-10 12:26:24 +0100

    AGGIUNTO IL FILE DELLA ROADMAP E RESO LE LEZIONI CLICCABILI

--- TableTalk/View/LessonsView.swift
+++ TableTalk/View/LessonsView.swift
@@ -14,42 +14,48 @@ struct LessonsView: View {
 
     var body: some View {
         let lessonList = model.lessonsList
-        
-        VStack {
-            Text("Lessons")
-                .font(Font.largeTitle)
-                .foregroundColor(.white)
-                .frame(maxWidth: .infinity)
-                .padding(.bottom,20)
-                .padding(.top,50)
-                .background(Color(r:182,g:23,b:45,opacity:100))
-            
-            Spacer()
-            
-            ScrollView {
-                VStack {
-                    ForEach(lessonList, id: \.self) { lesson in
-                        HStack {
-                            Text(lesson.number)
-                                .font(.title)
-                                .frame(width: 40, height: 50)
-                                .foregroundColor(.white)
-                                .background(
-                                    Color(red: 182/255, green: 23/255, blue: 45/255)
-                                )
+NavigationStack{
+            VStack {
+                Text("Lessons")
+                    .font(Font.largeTitle)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding(.bottom,20)
+                    .padding(.top,50)
+                    .background(Color(r:182,g:23,b:45,opacity:100))
+                
+                Spacer()
+                
+                ScrollView {
+                    VStack {
+                        ForEach(lessonList, id: \.self) { lesson in
+                            NavigationLink{
+                                RoadMapView()
+                            } label: {
+                                HStack {
+                                    Text(lesson.number)
+                                        .font(.title)
+                                        .frame(width: 40, height: 50)
+                                        .foregroundColor(.white)
+                                        .background(
+                                            Color(red: 182/255, green: 23/255, blue: 45/255)
+                                        )
+                                    
+                                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                                    
+                                    Text(lesson.title.uppercased())
+                                    .foregroundColor(.black)
+                                    Spacer()
+                                }
+                                .padding()
+                                .background(Color(.systemGray5))
                                 .clipShape(RoundedRectangle(cornerRadius: 12))
-                            
-                            Text(lesson.title.uppercased())
-                            Spacer()
+                            }
                         }
-                        
-                        .padding()
-                        .background(Color(.systemGray5))
-                        .clipShape(RoundedRectangle(cornerRadius: 12))
                     }
                 }
+                .background(Color(r: 255,g: 247,b: 238,opacity: 100))
             }
-            .background(Color(r: 255,g: 247,b: 238,opacity: 100))
         }
     }
 }
---
+++ TableTalk/View/RoadMapView.swift
@@ -0,0 +1,9 @@
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct RoadMapView: View {
+    var body: some View {
+    }
+}
