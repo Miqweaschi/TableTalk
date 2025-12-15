f6fc4508b65bd18486447ac8ceb851e453caaa8e
francesco <ecrasevvv@gmail.com>
2025-12-15 14:41:25 +0100

    SubView per esercizio

---
+++ TableTalk/View/SubEsercizioView.swift
@@ -0,0 +1,18 @@
+//
+//  SubEsercizioView.swift
+//  TableTalk
+//
+//  Created by AFP PAR 32 on 15/12/25.
+//
+
+import SwiftUI
+
+struct SubEsercizioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SubEsercizioView()
+}
