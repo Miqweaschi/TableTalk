256f562545339226671745c448b7f38991adbbf6
Matteo <matteoromano64@icloud.com>
2025-12-15 15:24:10 +0100

    Simulayion View

--- TableTalk/View/SimulationView.swift
+++ TableTalk/View/SimulationView.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import Combine
+import SwiftUI
+
+
+struct SimulationView:  View {
+    
+    @EnvironmentObject var model : Model
+    
+    var body : some View {
+        VStack{
+            
+            
+            Text("Simulation")
+                .font(Font.largeTitle)
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .padding(.bottom,20)
+                .padding(.top,50)
+                .background(Color(r:182,g:23,b:45,opacity:100))
+            
+            Spacer()
+        }
+        .background(Color(r: 255,g: 247,b: 238,opacity: 100))
+    }
+}
+
+#Preview {
+    SimulationView()
+}
